16a75cb4ad34ba621c2348141df07bad1257031e
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-08-12 20:28:18 +0300

    build: next image in storybook

---
+++ next_app/.storybook/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = ({ config }) => {
+    config.resolve.alias = {
+      "next/image": require.resolve("./__mocks__/NextJSImageMock.js"),
+    };
+  
+    return config;
+  };
+  
--- next_app/next.config.js
+++ next_app/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    // domains: ['storage.googleapis.com', 'coinpayments.net'],
+    minimumCacheTTL: 1500000,
+  },
+  compiler: {
+    removeConsole: false,
+  },
   swcMinify: true,
 };
 
