13ae460385e0d02a42a3205f03fb14e5ab82b1c9
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-08-25 12:41:50 +0300

    build: settings

--- next_app/next.config.js
+++ next_app/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_SECRET_KEY: process.env.NEXT_PUBLIC_SECRET_KEY,
+  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+      
+  //   }
+  // },
   images: {
     // domains: ['storage.googleapis.com', 'coinpayments.net'],
     minimumCacheTTL: 1500000,
@@ -9,6 +22,7 @@ const nextConfig = {
     removeConsole: false,
   },
   swcMinify: true,
+
 };
 
 module.exports = nextConfig;
